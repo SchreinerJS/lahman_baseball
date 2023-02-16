373d4b66db9884222159898515227340977b73b8
Richard Leonard <richard.z.leonard@gmail.com>
2023-02-16 12:11:11 -0600

    final push with edits

--- question_9_rl.sql
+++ question_9_rl.sql
@@ -1,6 +1,24 @@
 --9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won
 --the award.
 
+SELECT playerid, --COUNT of awards per manager with >1 award
+	   COUNT(*)
+FROM awardsmanagers
+WHERE awardid = 'TSN Manager of the Year'
+	AND (lgid = 'NL' OR lgid = 'AL')
+GROUP BY playerid
+HAVING COUNT(*) > 1
+ORDER BY playerid
+
+
+SELECT * -- single table sorting by correct award and either 'NL' or 'AL' league id
+FROM awardsmanagers
+WHERE awardid = 'TSN Manager of the Year'
+	AND (lgid = 'NL' OR lgid = 'AL')
+ORDER BY playerid
+
+
+
 WITH am_al_nl AS (SELECT DISTINCT namefirst, --CTE with first/last names of managers who recieved both NL and AL 'TSN Manager of the Year' awards 
 	   							  namelast, 
 	   							  UNNEST(ARRAY[am_nl.playerid, am_al.playerid]) AS am_playerid, --stacked manager playerids for NL and AL
