d9b722850d9f01c6fb418e41349c76ffd71b42f7
Jennifer Schreiner <jennifer.rebecca.schreiner@gmail.comgit commit -m initial>
2023-02-16 12:59:47 -0600

    modified final push

--- Question_2_js.sql
+++ Question_2_js.sql
@@ -1,58 +1,27 @@
 -- 2. a. Find the name and height of the shortest player in the database. 
 
--- people, appearance, teams
--- height = inches
--- some height fields are null
--- JOIN to appearances on playerid, JOIN to teams on teamid
--- Choice made by team to use namegiven (legal name) versus namefirst (nickname)
---subquery ORDER BY may not carry over to outer query, so much be in outer query
-
-SELECT namegiven, namelast, MIN(height) AS shortest_height
+SELECT namegiven, namelast, height
 FROM people
-GROUP BY namegiven, namelast
-ORDER BY shortest_height
-LIMIT 1;
-
-SELECT namegiven, namelast, height AS shortest_height
-FROM people
-WHERE height IS NOT NULL
-ORDER BY shortest_height
-LIMIT 1;
+WHERE height = (SELECT MIN(height) AS shortest_height
+				FROM people);
 
 --2b. How many games did he play in? What is the name of the team for which he played?
-SELECT namegiven,
-		namefirst,
-		namelast,
-		height AS shortest_height,
-		name AS team,
-		g_all 
+
+SELECT namegiven, namelast, height AS shortest_height, name AS team, g_all
 FROM people
 LEFT JOIN appearances USING (playerid)
 LEFT JOIN teams USING (teamid)
-WHERE height IS NOT NULL
+WHERE height = (SELECT MIN(height)
+				FROM people)
 ORDER BY shortest_height
 LIMIT 1;
 
-SELECT namegiven,
-		namefirst,
-		namelast,
-		MIN(height) AS min_height,
-		name AS team,
-		g_all AS games_played 
-FROM people
-LEFT JOIN appearances USING (playerid)
-LEFT JOIN teams USING (teamid)
-WHERE height IS NOT NULL
-GROUP BY namegiven,
-		namefirst,
-		namelast,
-		name,
-		g_all
-ORDER BY min_height
-LIMIT 1;
 ----------------------------------------------------
 --ANSWER: Player: Edward Carl "Eddie" Gaedel, 43" tall (3'7")) Team - St. Louis Browns, Games Played = 1,    
 ---------------------------------------------------
+
+
+
 --CHECKS:
 SELECT *
 FROM people
@@ -65,3 +34,13 @@ WHERE playerid = 'gaedeed01'
 
 -- https://en.wikipedia.org/wiki/Eddie_Gaedel
 -- 'Edward Carl Gaedel (June 8, 1925 – June 18, 1961) was the smallest player to appear in a Major League Baseball game' at 3'4''
+
+------------------
+--NOTES:
+-- Thought players often go by their nicknames, the team decided to use their given name in this query.
+-- people, appearance, teams
+-- height = inches
+-- some height fields are null
+-- JOIN to appearances on playerid, JOIN to teams on teamid
+-- Choice made by team to use namegiven (legal name) versus namefirst (nickname)
+-- subquery ORDER BY may not carry over to outer query, so much be in outer query
