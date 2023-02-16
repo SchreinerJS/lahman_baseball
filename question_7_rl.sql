06cc72f0800cc7b1f6d06c3e9e3fb2b5015590a5
Richard Leonard <richard.z.leonard@gmail.com>
2023-02-16 12:22:57 -0600

    final push with edits

--- question_7_rl.sql
+++ question_7_rl.sql
@@ -21,12 +21,34 @@ WHERE wswin = 'Y'
 ORDER BY w;
 --75 wins in 1918 by BOS
 
-SELECT yearid, teamID, w, l , wswin, -- shows w/l of world series winners per year compared to max win (w_max) for that year
-	   MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) AS w_max, --w_max which is partitioned by year 
+SELECT yearid, -- shows w/l of world series winners per year compared to max win (w_max) for that year
+	   teamID, 
+	   w, 
+	   l , 
+	   wswin, 
+	   MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) AS w_max, --max wins partitioned by year as w_max
 	   CASE WHEN w = MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) THEN 'Y'
 	   		WHEN w <> MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) THEN NULL
-			END AS ws_winner_is_max --ws_winner_is_max column that returns 'Y' if the w column matches the w_max column for that year 
+			END AS ws_winner_is_max --ws_winner_is_max column that returns 'Y' if the w column for the world series winning team that year matches the w_max column for that year 
 FROM teams
 WHERE yearid BETWEEN 1970 AND 2016
-	AND wswin = 'Y' --filtered for only world series winners per year
---from 1970-2016 the world series winner also had the most wins 100% of the time
+	AND wswin = 'Y'; --filtered for only world series winners per year
+
+
+WITH ws_winning_teams AS (SELECT yearid, --CTE of query above
+						  		 teamID, 
+						  		 w, 
+						  		 l , 
+						  		 wswin, 
+	   					  		 MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) AS w_max, 
+	   					  		 CASE WHEN w = MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) THEN 'Y'
+	   								  WHEN w <> MAX(w)OVER(PARTITION BY yearid ORDER BY yearid) THEN NULL
+								  	  END AS ws_winner_is_max 
+						  FROM teams
+						  WHERE yearid BETWEEN 1970 AND 2016
+						  	AND wswin = 'Y')
+SELECT (COUNT(ws_winner_is_max)/COUNT(*))*100 AS percent_highest_win_ws
+FROM ws_winning_teams
+WHERE ws_winner_is_max = 'Y';
+
+--from 1970-2016 the world series winner also had the most wins for that year 100% of the time
