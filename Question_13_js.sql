38185430b71d1c3db4edec426f6ebb26d2b612e2
Jennifer Schreiner <jennifer.rebecca.schreiner@gmail.comgit commit -m initial>
2023-02-16 16:01:13 -0600

    partial answer

--- Question_13_js.sql
+++ Question_13_js.sql
@@ -11,22 +11,81 @@
 --LESS EFFECTIVE wild pitches, batters hit by pitch, walks
 
 
---GOAL: Pull ids for pitchers only
-SELECT playerid
+--Pull ids for pitchers only
+-- BFP Batters faced by Pitcher
+
+WITH count_of_handedness AS (WITH pitchers AS (SELECT DISTINCT playerid, SUM(g) AS games_pitched
+											FROM pitching
+											WHERE g > 50   -- need this to be an AVG
+											GROUP BY playerid)
+
+					SELECT COUNT(*) AS total_pitchers,
+					COUNT(CASE WHEN throws LIKE 'R' THEN throws END) AS right_handed,
+					COUNT(CASE WHEN throws LIKE 'L' THEN throws END) AS left_handed
+					FROM pitching
+					INNER JOIN people
+					USING(playerid))
+
+SELECT 	ROUND(((right_handed::numeric / total_pitchers::numeric)*100), 2) AS percentage_right_handed,
+		ROUND(((left_handed::numeric / total_pitchers::numeric)*100), 2)  AS percentage_left_handed
+FROM count_of_handedness
+
+-- About 27.8% left handed pitchers, compared to the general population which is less than 3% strongly/completely left-handed.  So it's more than 9 times more likely to find a left-handed pitcher versus a left-handed person in society.
+
+--https://www.rightleftrightwrong.com/statistics.html#:~:text=Most%20people%20fall%20towards%20the,evenly%20mixed%2Dhanded%20or%20ambidextrous.
+
+SELECT playerid, awardid
+FROM awardsshareplayers
+WHERE awardid ILIKE 'cy%'
+	
+--total games pitched = 1064803
+										  
+	total_games_pitched AS (SELECT DISTINCT playerid, SUM(g) AS games_pitched
+							FROM pitching
+							WHERE g > 0
+							GROUP BY playerid)
+		SELECT SUM(games_pitched)
+		FROM pitchers
+SELECT 
+
 FROM people
---19112
+WHERE playerid = (SELECT playerid
+	  			 FROM fielding
+				 WHERE pos LIKE 'P')
+GROUP BY playerid
 
-SELECT DISTINCT playerid
-FROM pitching
 ORDER BY playerid
---9302 -- is this just pitchers? just less than half the total number of people in database
 
-SELECT playerid, pos
+SELECT 	DISTINCT playerid AS total_pitchers,
+
+FROM people
+INNER JOIN pitching
+
+
+people, throws = handed for throwing
+
+
+
+
+----------------
+--SCRATCH PAD
+--G_p = games as pitcher
+--G == pitching, games, some fields are 0
+
+SELECT COUNT (DISTINCT playerid)
 FROM fielding
 WHERE pos LIKE 'P'
 --9302 -- is this just pitchers? just less than half the total number of people in database
 
---This doesn't seem right
+SELECT playerid, pos, SUM(G_p) AS total_games_pitched
+FROM fielding
+INNER JOIN appearances USING(playerid)
+WHERE pos LIKE 'P' AND
+	G_p > 0
+GROUP BY playerid, pos
+ORDER BY total_games_pitched DESC
+
+--Is this right for total games pitched?
 SELECT playerid, SUM(G_p) AS total_games_pitched
 FROM appearances
 WHERE G_p > 0
@@ -35,34 +94,7 @@ ORDER BY total_games_pitched DESC
 --9302 pitchers 
 --G_p = games as pitcher
 
-AS total_games_as_pitcher
-
-appearances g_p games as pitcher
-
-SELECT COUNT (DISTINCT playerid)
-FROM fielding
-WHERE pos LIKE 'P'
---9302 -- really?
-
-
-
-SELECT 	DISTINCT playerid AS total_pitchers,
-		COUNT(CASE WHEN throws LIKE 'R' THEN throws END) AS right_handed,
-		COUNT(CASE WHEN throws LIKE 'L' THEN throws END) AS left_handed,
-FROM people
-INNER JOIN pitching
-
-		DISTINCT playerid AS total_distinct_players
-			(right_handed / DISTINCT playerid) AS percentage_left_handed
-
-
-people, throws = handed for throwing
-
 --Named for the winningest pitcher in baseball history, the Cy Young Award is voted upon by the Baseball Writers' Association of America prior to the beginning of the postseason. From 1956 to 1966, the award was given to one pitcher, but has been given to one pitcher per league since 1967. The vote totals are based on a weighted points system.
-
-SELECT playerid, awardid
-FROM awardsshareplayers
-WHERE awardid ILIKE 'cy%'
 
 -- Pitching table
 
