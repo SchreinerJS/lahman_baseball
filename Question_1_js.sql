e5f696595cdd9b8f734e4a351f50f05b70b24822
Jennifer Schreiner <jennifer.rebecca.schreiner@gmail.comgit commit -m initial>
2023-02-16 12:40:09 -0600

    modified final push

--- Question_1_js.sql
+++ Question_1_js.sql
@@ -1,21 +1,18 @@
 -- 1. What range of years for baseball games played does the provided database cover? 
 
---NOTE: (from data dictionary) This database contains pitching, hitting, and fielding statistics for Major League Baseball from 1871 through 2016.  It includes data from the two current leagues (American and National), the four other "major" leagues (American Association, Union Association, Players League, and Federal League), and the National Association of 1871-1875. 
-
 SELECT MIN(yearid) AS min_year, MAX(yearid) AS max_year
-FROM teams
+FROM teams;
 
---ANSWER: RANGE OF YEARS THAT TEAMS EXISTED = 1871 to 2016
+--ANSWER: The range of years for baseball agmes played in this database is 1871 to 2016.
 
---Year in teams relative to leagues and games played
 
 SELECT g, MAX(yearid) AS earliest_year_game_played
 FROM teams
 GROUP BY g
 ORDER BY earliest_year_game_played
-LIMIT 1;
+LIMIT 1;  --Year in teams relative to leagues and games played
 
---ANSWER: EARLIEST GAMES WERE PLAYED IN 1871
+--ANSWER: Earliest game played in the databse was in 1871.
 
 SELECT g, MAX(yearid) AS latest_year_game_played
 FROM teams
@@ -23,4 +20,9 @@ GROUP BY g
 ORDER BY latest_year_game_played DESC
 LIMIT 1;
 
---ANSWER: LATEST GAMES WERE PLAYED IN 2016
+--ANSWER: Latest game played in database was in 2016.
+
+-------------------------------------------------------
+--NOTE: (from data dictionary) This database contains pitching, hitting, and fielding statistics for Major League Baseball from 1871 through 2016.  It includes data from the two current leagues (American and National), the four other "major" leagues (American Association, Union Association, Players League, and Federal League), and the National Association of 1871-1875. 
+
+--g=games played
